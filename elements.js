35d0d5e89fee45776b590f2dfe716ee13675ca79
Nicki Christian <nicki.christian@bryanuniversity.edu>
2020-06-23 16:47:56 -0500

    commit

---
+++ elements.js
@@ -0,0 +1,14 @@
+var h1 = document.createElement("h1")
+document.getElementById('someText')
+.innerHTML = 'Welcome to my JS site';
+
+var p = document.createElement("p")
+document.getElementById('Text')
+.innerHTML = "All of this was created with JavaScript";
+
+
+var newLi = document.createElement("li")
+newLi.textContent = "eveyones" 
+var myList = document.getElementById("my-list")
+myList.append(newLi);
+
